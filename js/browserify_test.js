fbc144d5ade41b998fd4a35cc693fc589a4de7b8
dslaugh <dslaugh@gmail.com>
2014-06-20 14:14:36 -0600

    not sure

--- js/browserify_test.js
+++
@@ -1,8 +0,0 @@
-var names = require('./names.js'),
-    foundSuperman = require('./findsuperman.js');
-
-if (foundSuperman(names())) {
-    document.write('Found Superman');
-} else {
-    document.write('No Superman');
-}
--- js/names.js
+++
@@ -1,3 +0,0 @@
-module.exports = function() {
-	return ['Bruce Wayne', 'Wally West', 'John Jones', 'Kyle Rayner', 'Arthur Curry', 'Clark Kent', 'Barry Allen', 'Hal Jordan', 'Kara Kent', 'Diana Prince', 'Ray Palmer', 'Oliver Queen'];
-};
