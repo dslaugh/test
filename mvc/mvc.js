ec6d4e5a70666fb39c958eb31f72f31611a563e9
dslaugh <dslaugh@gmail.com>
2016-03-10 09:55:03 -0700

    updated mvc and foosball

--- mvc/mvc.js
+++ mvc/mvc.js
@@ -23,20 +23,20 @@ var Model = {
 	events: CustomEvents(),
 	addName: function(name) {
 		this.names.push(name);
-		this.events.emit('name-added');
+		this.events.emit('name-added', Model.names);
 	},
 	removeName: function(name) {
 		var nameToRemoveIndex = this.names.indexOf(name);
 		this.names.splice(nameToRemoveIndex, 1);
-		this.events.emit('name-removed');
+		this.events.emit('name-removed', Model.names);
 	}
 };
 
 var View = {
 	nameInput: document.querySelector('#name'),
 	nameList: document.querySelector('#names'),
-	renderNames: function() {
-		View.nameList.innerHTML = Model.names.reduce(function(prev, curr) {
+	renderNames: function(names) {
+		View.nameList.innerHTML = names.reduce(function(prev, curr) {
 			return prev + '<li class="names-item">' + curr + '</li>';
 		}, '');
 	},
@@ -47,23 +47,25 @@ var View = {
 		this.nameInput.addEventListener('change', Controller.addName);
 		this.nameList.addEventListener('dblclick', function(e) {
 			if (e.target.className.match(/names-item/)) {
-				Model.removeName(e.target.innerText);
+				Controller.removeName(e);
 			}
 		});
 	}
 };
 
 var Controller = {
+	initialize: function() {
+		Model.events.on('name-added', View.renderNames);
+		Model.events.on('name-removed', View.renderNames);
+		Model.events.on('name-added', View.resetNameInput);
+		View.bindUIEvents();
+	},
 	addName: function(e) {
 		Model.addName(e.target.value);
+	},
+	removeName: function(e) {
+		Model.removeName(e.target.innerText);
 	}
 };
 
-function initialize() {
-	Model.events.on('name-added', View.renderNames);
-	Model.events.on('name-removed', View.renderNames);
-	Model.events.on('name-added', View.resetNameInput);
-	
-	View.bindUIEvents();
-}
-initialize();
+Controller.initialize();
