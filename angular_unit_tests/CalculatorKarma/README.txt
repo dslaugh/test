6c4d93f4ca355d5f029247e3cabfa14031d72f7f
dslaugh <dslaugh@gmail.com>
2016-04-29 13:22:32 -0600

    angular testing with karma

---
+++ angular_unit_tests/CalculatorKarma/README.txt
@@ -0,0 +1,2 @@
+http://www.bradoncode.com/tutorials/angularjs-unit-testing/
+http://www.bradoncode.com/blog/2015/05/19/karma-angularjs-testing/
---
+++ angular_unit_tests/js/simpleService.js
@@ -0,0 +1,17 @@
+angular
+.module('app')
+.factory('SimpleService', SimpleService);
+
+SimpleService.$inject = ['$log'];
+
+function SimpleService($log) {
+    var service = {
+        DoSomething: doSomething,
+    };
+
+    return service;
+
+    function doSomething() {
+        $log.info('something done');
+    }
+}
